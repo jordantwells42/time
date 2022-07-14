bcf858a617a9093bb84a4f60d5a0f3d85c1a9d79
Jordan Wells <jordantwells@gmail.com>
2022-07-13 20:28:09 -0400

    trying to fix favicon color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -62,7 +62,7 @@ const Home: NextPage = () => {
 
 <svg version="1.1" id="circle" color={color.toHexString()} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 width="15px" height="15px" viewBox="0 0 15 15"  xmlSpace="preserve">
-<path color={color.toHexString()} d="M14,7.5c0,3.5899-2.9101,6.5-6.5,6.5S1,11.0899,1,7.5S3.9101,1,7.5,1S14,3.9101,14,7.5z"/>
+<path stroke={color.toHexString()} fill={color.toHexString()} color={color.toHexString()} d="M14,7.5c0,3.5899-2.9101,6.5-6.5,6.5S1,11.0899,1,7.5S3.9101,1,7.5,1S14,3.9101,14,7.5z"/>
 </svg>
 
 
